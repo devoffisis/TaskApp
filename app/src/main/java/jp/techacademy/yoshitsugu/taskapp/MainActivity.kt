66395811fc02958ff6fa99740eb1c77a2c7be1e5
Yoshitsugu Sekine <yoshitsugu.sekine@offisis.co.jp>
2022-07-20 16:52:33 +0900

    Chapter6.5 temporal

--- app/src/main/java/jp/techacademy/yoshitsugu/taskapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yoshitsugu/taskapp/MainActivity.kt
@@ -1,19 +1,16 @@
 package jp.techacademy.yoshitsugu.taskapp
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.ListView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import com.google.android.material.snackbar.Snackbar
 import io.realm.Realm
 import io.realm.RealmChangeListener
 import io.realm.Sort
 import jp.techacademy.yoshitsugu.taskapp.databinding.ActivityMainBinding
-import java.util.*
+
+const val EXTRA_TASK = "jp.techacademy.yoshitsugu.sekine.taskapp.TASK"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mRealm: Realm
@@ -28,16 +25,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        // val listView1 = findViewById<ListView>(R.id.listView1)
-
+        val listView1 = findViewById<ListView>(R.id.listView1)
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            val intent = Intent(this, InputActivity::class.java)
+            startActivity(intent)
         }
 
         // Realmの設定
@@ -47,19 +43,43 @@ class MainActivity : AppCompatActivity() {
         // ListViewの設定
         mTaskAdapter = TaskAdapter(this)
 
-
         /*
         listView1.setOnItemClickListener {parent, view, position, id ->
-            true
+            // 入力・編集する画面に遷移させる
+            val task = parent.adapter.getItem(position) as Task
+            val intent = Intent(this, InputActivity::class.java)
+            intent.putExtra(EXTRA_TASK, task.id)
+            startActivity(intent)
         }
 
         listView1.setOnItemLongClickListener{parent, view, position, id ->
+            // タスクを削除する
+            val task = parent.adapter.getItem(position) as Task
+
+            // ダイアログを表示する
+            val builder = AlertDialog.Builder(this)
+
+            builder.setTitle("削除")
+            builder.setMessage(task.title + "を削除しますか")
+
+            builder.setPositiveButton("OK"){_, _ ->
+                val results = mRealm.where(Task::class.java).equalTo("id", task.id).findAll()
+
+                mRealm.beginTransaction()
+                results.deleteAllFromRealm()
+                mRealm.commitTransaction()
+
+                reloadListView()
+            }
+
+            builder.setNegativeButton("CANCEL", null)
+
+            val dialog = builder.create()
+            dialog.show()
+
             true
         }
          */
-
-        addTaskForTest()
-
         reloadListView()
     }
 
@@ -71,7 +91,6 @@ class MainActivity : AppCompatActivity() {
 
         // 上記の結果を、TaskListとしてセットする
         mTaskAdapter.mTaskList = mRealm.copyFromRealm(taskRealmResults)
-
         listView1.adapter = mTaskAdapter
         mTaskAdapter.notifyDataSetChanged()
     }
@@ -81,39 +100,4 @@ class MainActivity : AppCompatActivity() {
 
         mRealm.close()
     }
-
-    private fun addTaskForTest() {
-        val task = Task()
-        task.title = "作業"
-        task.contents = "プログラムを書いてPUSHする"
-        task.date = Date()
-        task.id = 0
-        mRealm.beginTransaction()
-        mRealm.copyToRealmOrUpdate(task)
-        mRealm.commitTransaction()
-    }
-
-    /*
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
-     */
 }
