1cd646267eb81bc872eaaccadcf45a2cbbc5736a
Yoshitsugu Sekine <yoshitsugu.sekine@offisis.co.jp>
2022-07-20 14:49:18 +0900

    Update

--- app/src/main/java/jp/techacademy/yoshitsugu/taskapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yoshitsugu/taskapp/MainActivity.kt
@@ -1,14 +1,13 @@
 package jp.techacademy.yoshitsugu.taskapp
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
 import jp.techacademy.yoshitsugu.taskapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,12 +20,6 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
